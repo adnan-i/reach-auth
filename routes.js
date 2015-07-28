81f68faa74cb21a9b571a71cb1126a35e8d0453b
Christoffer Rødvik <christoffer@kodemon.net>
2015-07-28 19:31:44 +0900

    Reach.Router => Route

--- routes.js
+++ routes.js
@@ -1,27 +1,25 @@
 'use strict';
 
-var router = Reach.Router;
-
-router.post('/auth/login', {
+Route.post('/auth/login', {
   uses   : 'AuthController@login',
   params : ['email', 'password']
 });
 
-router.post('/auth/facebook', {
+Route.post('/auth/facebook', {
   uses   : 'AuthController@facebook',
   params : ['code', 'redirectUri']
 });
 
-router.get('/auth/remember', {
+Route.get('/auth/remember', {
   policy : 'authenticate',
   uses   : 'AuthController@remember'
 });
 
-router.get('/auth/validate', {
+Route.get('/auth/validate', {
   policy : 'authenticate',
   uses   : 'AuthController@validate'
 });
 
-router.get('/auth/logout', {
+Route.get('/auth/logout', {
   uses : 'AuthController@logout'
 });
