71af4c2684b28dc2a955b041bba5bd65e8f9b045
Christoffer Rødvik <christoffer@kodemon.net>
2015-08-25 20:41:05 +0900

    facebook now acts on post

--- routes.js
+++ routes.js
@@ -5,10 +5,7 @@ Route.pst('/auth/login', {
   params : ['email', 'password']
 });
 
-Route.get('/auth/facebook', {
-  uses : 'AuthController@facebook'
-});
-
+Route.pst('/auth/facebook',                  'AuthController@facebook');
 Route.get('/auth/remember', ['authenticate', 'AuthController@remember']);
 Route.get('/auth/validate', ['authenticate', 'AuthController@validate']);
 Route.get('/auth/logout',                    'AuthController@logout');
